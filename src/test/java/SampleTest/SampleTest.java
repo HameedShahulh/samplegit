3bbfb9aac59e700dd68c632c23cee196f387f78c
Hameedshahulh <hameed15shahulh@gmail.com>
2018-10-27 17:55:55 +0530

    Thirt commit

---
+++ src/test/java/SampleTest/SampleTest.java
@@ -0,0 +1,27 @@
+package SampleTest;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	@Test
+	public void loginTest() throws IOException {
+
+		WebDriver wd = new FirefoxDriver();
+		wd.manage().window().maximize();
+		wd.get("https://www.bing.com/");
+		WebElement login = wd.findElement(By.name("q"));
+		login.sendKeys("abc");
+		File src = ((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src, new File("C:\\Users\\Admin\\workspace\\GitHubTest\\Screenshot\\praveen.jpg"));
+		}
+}
